69e6d5f1dc7ff3cfd29b7c08f9898c276082d5de
Mateus <mateuspsilva.dev@gmail.com>
2020-11-05 18:08:05 -0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs').promises;
+const path = require('path');
 
-var fileName = '', size=0;
+async function listarArquivosDoDiretorio(diretorio, arquivos) {
 
-fs.readdir(searchpath, (err, files) => {
-    if(err){
-        console.log('Could not list the directory files'. err);
+    if (!arquivos)
+        arquivos = [];
+
+    relations = {
+        '10343941977.jpg': '10343941977',
+        '10343941984.jpg': '10343941984',
+        '10343941991.jpg': '10343941991',
+        '10343942004.jpg': '10343942004',
+        '26388226436.jpg': '26388226436',
+        '26388287291.jpg': '26388287291',
+    };
+
+    let listaDeArquivos = await fs.readdir(diretorio);
+
+    console.log(listaDeArquivos);
+
+    for (let k in listaDeArquivos) {
+        name = path.basename(k.name, '.jpg');
+        console.log(name);
+
+        // let stat = await fs.stat(diretorio + '/' + listaDeArquivos[k]);
+        // if(stat.isDirectory())
+        // await listarArquivosDoDiretorio(diretorio + '/' + listaDeArquivos[k], arquivos);
+        // else
+        // arquivos.push(diretorio + '/' + listaDeArquivos[k]);
     }
 
-    files.forEach(function(file, index){
-        var fromPath = path.join(searchpath, file);
-        var fileSize = getFileSizeInBytes(fromPath);
+    // return arquivos;
 
-        fileName = file;
-        size = fileSize;
-
-        console.log('File name: ' + $filename + ' | File size: ' + $size);
-    });
-});
-
-function getFileSizeInBytes(filename){
-    var stats = fs.statSync(filename);
-    var fileSizeInBytes = stats['size'];
-    return fileSizeInBytes;
 }
+
+listarArquivosDoDiretorio('./imagens');
+
+// async function test() {
+//     let arquivos = await listarArquivosDoDiretorio('./imagens'); // coloque o caminho do seu diretorio
+//     console.log(arquivos);
+//     return arquivos;
+// }
+
+// test();
